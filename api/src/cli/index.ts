67c89308c539c62912504c52705c92bec8b050c5
Will K <dev@pxslip.com>
2024-06-17 18:21:47 +0000

    Remove directus reference from the cli

--- api/src/cli/index.ts
+++ api/src/cli/index.ts
@@ -25,11 +25,11 @@ export async function createCli(): Promise<Command> {
 
 	await emitter.emitInit('cli.before', { program });
 
-	program.name('directus').usage('[command] [options]');
+	program.name('db-studio').usage('[command] [options]');
 	program.version(pkg.version, '-v, --version');
 
-	program.command('start').description('Start the Directus API').action(startServer);
-	program.command('init').description('Create a new Directus Project').action(init);
+	program.command('start').description('Start the DB Studio API').action(startServer);
+	program.command('init').description('Create a new DB Studio Project').action(init);
 
 	// Security
 	const securityCommand = program.command('security');
